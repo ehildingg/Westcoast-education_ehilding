d15d746fbdeb107e8d4d844cec0720d386379208
ehilding <erik.hildingsson@gmail.com>
2022-01-23 19:04:50 +0100

    Added background image to the different views, and made models for category and course

--- Westcoast-education_ehilding/Models/CategoryModel.swift
+++ Westcoast-education_ehilding/Models/CategoryModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct Category {
+    var categoryName: String
+    var categoryDescription: String
+    var categoryID: Int
+    var categoryImage: String
+    var courses: [Courses]
+    
+    
+    init(categoryName: String, categoryDescription: String, categoryID: Int, image: String, courses: [Courses]) {
+        self.categoryName = categoryName
+        self.categoryDescription = categoryDescription
+        self.categoryID = categoryID
+        self.categoryImage = image
+        self.courses = courses
+       
+    }
+}
--- Westcoast-education_ehilding/Models/CourseModel.swift
+++ Westcoast-education_ehilding/Models/CourseModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct Courses: Hashable {
+    var courseName: String
+    var courseDescription: String
+    var courseID: Int
+    var courseImage: String
+    
+    init(name: String, description: String, id: Int, image: String) {
+        
+        self.courseName = name
+        self.courseDescription = description
+        self.courseID = id
+        self.courseImage = image
+    }
+}
