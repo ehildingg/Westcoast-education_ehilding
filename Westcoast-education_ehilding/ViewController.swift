151f09593a8c7def0df62f5b3494a51e0c29fa13
ehilding <erik.hildingsson@gmail.com>
2022-01-23 17:15:40 +0100

    Initial Commit

---
+++ Westcoast-education_ehilding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Westcoast-education_ehilding
+//
+//  Created by Erik Hildingsson on 2022-01-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
