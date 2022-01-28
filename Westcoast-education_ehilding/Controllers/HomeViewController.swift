426b29d0fe8fe5a043276375f02bbeb3cd770360
ehilding <erik.hildingsson@gmail.com>
2022-01-28 15:59:54 +0100

    Added homescreen with background and button

---
+++ Westcoast-education_ehilding/Controllers/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  Westcoast-education_ehilding
+//
+//  Created by Erik Hildingsson on 2022-01-28.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
